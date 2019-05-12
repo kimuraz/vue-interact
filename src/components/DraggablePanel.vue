eb409ff5e1f661fab36126254212488c93ccf692
Kimura <joseduardo.kimura@gmail.com>
2019-05-12 15:22:13 -0300

    :star: Add DraggablePanel component

---
+++ src/components/DraggablePanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="draggable">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { DraggingMixin } from '../vue-interact';
+
+export default {
+  name: 'draggablePanel',
+  mixins: [DraggingMixin],
+};
+</script>
