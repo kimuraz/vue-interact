6dcd80ddec30cc4db87419bef05d5e7c31a9bd43
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-11-10 21:49:52 +0100

    :arrow_up: Replace object assign on draggable

--- src/directives/draggable.js
+++ src/directives/draggable.js
@@ -8,24 +8,22 @@ const bind = (el, binding, vnode) => {
 
   const pos = { x: 0, y: 0 };
 
-  const options = Object.assign(
-    {
-      listeners: {
-        start(event) {
-          vnode.context.$emit('start', event);
-        },
-        move(event) {
-          pos.x += event.dx;
-          pos.y += event.dy;
+  const options = {
+    listeners: {
+      start(event) {
+        vnode.context.$emit('start', event);
+      },
+      move(event) {
+        pos.x += event.dx;
+        pos.y += event.dy;
 
-          // eslint-disable-next-line no-param-reassign
-          el.style.transform = `translate(${pos.x}px, ${pos.y}px)`;
-          vnode.context.$emit('move', event);
-        },
+        // eslint-disable-next-line no-param-reassign
+        el.style.transform = `translate(${pos.x}px, ${pos.y}px)`;
+        vnode.context.$emit('move', event);
       },
     },
-    value,
-  );
+    ...value,
+  };
 
   interact(`.${className}`).draggable(options);
 };
