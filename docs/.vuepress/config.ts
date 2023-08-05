a4095b93a02c8017da57d6bad56c6278c8ee36ba
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2023-08-05 14:33:52 +0200

    Add new documentation based on vuepress 2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,63 +1,11 @@
-module.exports = {
-  serviceWorker: false,
-  locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'Vue Interact',
-      description: 'A VueJS wrapper for InteractJS',
-    },
-  },
-  themeConfig: {
-    repo: 'kimuraz/vue-interact',
-    docsDir: 'docs',
-    editLinks: true,
-    locales: {
-      '/': {
-        label: 'English',
-        selectTexts: 'Lang',
-        nav: [
-          {
-            text: 'Mixins',
-            items: [
-              {
-                text: 'DraggingMixin',
-                link: '/mixins/dragging',
-              },
-              {
-                text: 'ResizingMixin',
-                link: '/mixins/resizing',
-              },
-            ],
-          },
-          {
-            text: 'Directives',
-            items: [
-              {
-                text: 'v-draggable',
-                link: '/directives/draggable',
-              },
-            ],
-          },
-        ],
-        sidebar: [
-          '/introduction',
-          '/setup',
-          {
-            title: 'Mixins',
-            collapsable: false,
-            children: [
-              '/mixins/common',
-              '/mixins/dragging',
-              '/mixins/resizing',
-            ],
-          },
-          {
-            title: 'Directives',
-            collapsable: false,
-            children: ['/directives/draggable'],
-          },
-        ],
-      },
-    },
-  },
-};
+import { defineUserConfig, defaultTheme } from "vuepress";
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "VueInteract",
+  description: "VueInteract is a Vue 3 wrapper for Interact.js",
+  theme: defaultTheme({
+    logo: '/assets/VueInteract.svg',
+  }),
+});
+
