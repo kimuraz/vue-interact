226fcd2f0c5070d3dce9c890ac3fb929e98137d0
Kimuraz <j.eduardokimura@icloud.com>
2024-08-20 17:18:18 +0200

    Update vuepress to v2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,11 +1,12 @@
-import { defineUserConfig, defaultTheme } from "vuepress";
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from '@vuepress/theme-default';
+import { viteBundler } from '@vuepress/bundler-vite';
 
 export default defineUserConfig({
   lang: "en-US",
   title: "VueInteract",
   description: "VueInteract is a Vue 3 wrapper for Interact.js",
-  theme: defaultTheme({
-    logo: '/assets/VueInteract.svg',
-  }),
+  theme: defaultTheme(),
+  bundler: viteBundler(),
 });
 
